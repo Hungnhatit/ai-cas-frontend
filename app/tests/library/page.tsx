0e1737210e9efffba1dee1c2c2f42e47b0d7d351
Hungnhatit <nhatpro204@gmail.com>
2025-10-15 15:23:42 +0700

    feat: add test library page

---
+++ app/tests/library/page.tsx
@@ -0,0 +1,12 @@
+import TestLibrary from '@/components/test/test-library'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <TestLibrary />
+    </>
+  )
+}
+
+export default page
