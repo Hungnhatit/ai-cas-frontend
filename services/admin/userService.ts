29ecca1ce6a4f3ef231c457a05438a2ffaee5d53
Hungnhatit <nhatpro204@gmail.com>
2025-10-28 20:10:40 +0700

    feat: add more user services

---
+++ services/admin/userService.ts
@@ -0,0 +1,33 @@
+import { api } from "@/lib/axios"
+
+export const userService = {
+  getUsers: async () => {
+    const res = await api.get('/user');
+    return res.data;
+  },
+
+  getUserById: async (user_id: number) => {
+    const res = await api.get(`/user/${user_id}`);
+    return res.data;
+  },
+
+  updateUser: async (user_id: number, data: any) => {
+    const res = await api.put(`/user/${user_id}`, data)
+    return res.data
+  },
+
+  softDeleteUser: async (user_id: number) => {
+    const res = await api.patch(`/user/${user_id}/soft-delete`)
+    return res.data
+  },
+
+  restoreUser: async (user_id: number) => {
+    const res = await api.patch(`/user/${user_id}/restore`)
+    return res.data
+  },
+
+  forceDeleteUser: async (user_id: number) => {
+    const res = await api.delete(`/user/${user_id}`)
+    return res.data
+  },
+}
