494a8cbc1eda63ee71cafc8a7f9bab5ca8d889ce
Hungnhatit <nhatpro204@gmail.com>
2025-11-08 16:18:08 +0700

    ui: remove unused component

--- components/test/test-detail/test-detail.tsx
+++ components/test/test-detail/test-detail.tsx
@@ -11,6 +11,8 @@ import { Input } from '@/components/ui/input';
 import { Test, TestQuestion } from '@/types/interfaces/model';
 import { formatDate } from '@/utils/formatDate';
 import { useRouter } from 'next/navigation';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import DetailQuestionList from './DetailQuestionList';
 
 interface TestDetailProps {
   test_id: number
@@ -38,128 +40,6 @@ const mockInstructor: Instructor = {
   ngay_cap_nhat: "2023-09-01T14:30:00Z",
 };
 
-
-// --- UI COMPONENTS ---
-const HighlightedText = ({ text, highlight }: { text: string; highlight: string }) => {
-  if (!highlight.trim()) {
-    return <span>{text}</span>;
-  }
-  const regex = new RegExp(`(${highlight})`, 'gi');
-  const parts = text.split(regex);
-  return (
-    <span>
-      {parts.map((part, i) =>
-        regex.test(part) ? (
-          <mark key={i} className="bg-yellow-300 dark:bg-yellow-500 text-black px-0.5 rounded">
-            {part}
-          </mark>
-        ) : (
-          <span key={i}>{part}</span>
-        )
-      )}
-    </span>
-  );
-};
-
-
-const QuestionCard = ({ question, index, searchTerm }: { question: TestQuestion, index: number, searchTerm: string }) => {
-  const [isExpanded, setIsExpanded] = useState(true);
-
-  const getQuestionTypeLabel = (type: TestQuestion['loai']) => {
-    switch (type) {
-      case 'trac_nghiem': return 'Trắc nghiệm';
-      case 'tu_luan': return 'Tự luận';
-      case 'dung_sai': return 'Đúng/Sai';
-      case 'tra_loi_ngan': return 'Trả lời ngắn';
-      default: return 'Không xác định';
-    }
-  };
-
-  const renderAnswer = () => {
-    switch (question.loai) {
-      case 'trac_nghiem':
-        return (
-          <ul className="space-y-3 mt-4">
-            {question.lua_chon && question.lua_chon.map((option, index) => (
-              <li key={index} className={
-                `flex items-center p-3 rounded-[3px] border border-gray-300 text-sm hover:bg-blue-50 cursor-pointer
-                 ${index.toString() === question.dap_an_dung
-                && 'bg-blue-100 border-blue-500 dark:bg-blue-900/50 dark:border-blue-700'}`} >
-                {String.fromCharCode(65 + index)}.
-                <span className="flex-grow">
-                  <HighlightedText text={option} highlight={searchTerm} />
-                </span>
-                {index.toString() === question.dap_an_dung && <CheckCircle className="w-5 h-5 text-blue-600 dark:text-blue-400 mr-3 flex-shrink-0" />}
-              </li>
-            ))
-            }
-          </ul >
-        );
-      case 'dung_sai':
-        return (
-          <div className="mt-4 space-y-3">
-            <div className={`flex items-center p-3 rounded-lg border text-sm ${question.dap_an_dung === true ? 'bg-green-50 border-green-500 dark:bg-green-900/50 dark:border-green-700' : 'bg-gray-50 border-gray-200 dark:bg-gray-700/50 dark:border-gray-600'}`}>
-              {question.dap_an_dung === true && <CheckCircle className="w-5 h-5 text-green-600 dark:text-green-400 mr-3 flex-shrink-0" />}
-              <span>Dung</span>
-            </div>
-            <div className={`flex items-center p-3 rounded-lg border text-sm ${question.dap_an_dung === false ? 'bg-green-50 border-green-500 dark:bg-green-900/50 dark:border-green-700' : 'bg-gray-50 border-gray-200 dark:bg-gray-700/50 dark:border-gray-600'}`}>
-              {question.dap_an_dung === false && <CheckCircle className="w-5 h-5 text-green-600 dark:text-green-400 mr-3 flex-shrink-0" />}
-              <span>Sai</span>
-            </div>
-          </div>
-        );
-      case 'tra_loi_ngan':
-      case 'tu_luan':
-        return (
-          <div className="mt-4 p-4 rounded-lg bg-green-50 border border-green-500 dark:bg-green-900/50 dark:border-green-700">
-            <p className="text-sm font-semibold text-green-800 dark:text-green-300 mb-2">Dap an dung:</p>
-            <p className="text-sm text-gray-800 dark:text-gray-200"><HighlightedText text={String(question.dap_an_dung)} highlight={searchTerm} /></p>
-          </div>
-        );
-      default: return null;
-    }
-  }
-
-  return (
-    <Card className="mb-6 pt-0 overflow-hidden gap-0 rounded-[3px] shadow-none border-gray-300">
-      <CardHeader className="flex items-center justify-between shadow-none bg-gray-100 dark:bg-gray-900/50 py-2">
-        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4">
-          <h4 className="font-bold text-md text-gray-800 dark:text-gray-100">
-            Câu hỏi {index + 1}
-          </h4>
-          <div className="flex items-center gap-4">
-            <Badge variant="outline">{getQuestionTypeLabel(question.loai)}</Badge>
-            <Badge variant="default">{question.diem} điểm</Badge>
-          </div>
-        </div>
-        <Button
-          onClick={() => setIsExpanded(!isExpanded)}
-          className="px-2 py-2 bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700 text-gray-600 dark:text-gray-300 rounded-full h-auto w-auto cursor-pointer">
-          {isExpanded ? <Minimize2 size={20} color='black' /> : <Maximize2 size={20} color='black' />}
-        </Button>
-      </CardHeader>
-      {isExpanded && (
-        <CardContent className="space-y-4">
-          <p className="text-gray-700 dark:text-gray-300 mt-4">
-            <HighlightedText text={question.cau_hoi} highlight={searchTerm} />
-          </p>
-          {renderAnswer()}
-          <div className="mt-4 p-4 rounded-[3px] bg-gray-100 dark:bg-gray-700/50 border border-dashed border-gray-300 dark:border-gray-600">
-            <p className="text-sm font-semibold text-gray-800 dark:text-gray-200 mb-2">
-              Giải thích:
-            </p>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-              {question.giai_thich
-                ? <HighlightedText text={question.giai_thich} highlight={searchTerm} />
-                : 'Không có giải thích chi tiết cho câu hỏi này'}
-            </p>
-          </div>
-
-        </CardContent>
-      )}
-    </Card>
-  )
-}
 
 const InfoCard = ({ icon: Icon, title, value, className = '' }: { icon: React.ElementType, title: string, value: number, className?: string }) => (
   <div className={`flex items-center gap-4 p-3 rounded-[3px] border border-gray-300 bg-gray-50 dark:bg-gray-700/50 ${className}`}>
@@ -189,15 +69,11 @@ const ExamDetailsPage = ({ test_id }: TestDetailProps) => {
       if (res.success) {
         setTest(res.data);
       }
-      console.log(res.data);
     }
     fetchTest();
   }, [test_id]);
 
-  console.log(test);
-
   const danh_muc = test ? JSON.parse(test?.danh_muc) : [];
-  console.log(danh_muc);
 
   const filteredQuestions = useMemo(() => {
     if (!searchTerm) return test?.cau_hoi;
@@ -213,13 +89,22 @@ const ExamDetailsPage = ({ test_id }: TestDetailProps) => {
     });
   }, [test?.cau_hoi, searchTerm]);
 
-  console.log(filteredQuestions);
+  const parsedQuestions = (test?.cau_hoi || []).map((q: any) => ({
+    ...q,
+    lua_chon: typeof q.lua_chon === 'string' ? JSON.parse(q.lua_chon) : q.lua_chon,
+    dap_an_dung: q.dap_an_dung !== undefined ? Number(q.dap_an_dung) : undefined,
+  }));
+
+  const mergedSections = (test?.phan_kiem_tra || []).map((section: any) => ({
+    ...section,
+    cau_hoi: parsedQuestions.filter((q: any) => q.ma_phan === section.ma_phan),
+  }));
 
   const getDifficultyBadge = (difficulty: Test['do_kho'] | null) => {
     switch (difficulty) {
-      case 'de': return <Badge variant='default' className="capitalize">De</Badge>;
+      case 'de': return <Badge variant='default' className="capitalize">Dễ</Badge>;
       case 'trung_binh': return <Badge variant='outline' className="capitalize">Trung bình</Badge>;
-      case 'kho': return <Badge variant='destructive' className="capitalize">Kho</Badge>;
+      case 'kho': return <Badge variant='destructive' className="capitalize">Khó</Badge>;
     }
   }
 
@@ -233,19 +118,22 @@ const ExamDetailsPage = ({ test_id }: TestDetailProps) => {
 
   return (
     <div className="bg-gray-50 dark:bg-gray-900 min-h-screen font-sans text-gray-900 dark:text-gray-100">
-      <div className=" mx-auto p-4">
+      <div className="">
         {/* Header */}
-        <header className="flex items-start justify-between mb-4">
+        <header className="bg-[#232f3e] -mx-8 -mt-8 p-8 flex items-start justify-between mb-4 text-white">
           <div>
-            <CardDescription className='text-lg'>Chi tiết bài kiểm tra</CardDescription>
-            <CardTitle className="text-3xl font-bold mt-1 mb-2">{test?.tieu_de}</CardTitle>
-            <div className='flex flex-wrap gap-2 items-center'>
+            <CardTitle className="text-2xl font-bold mt-1 mb-2">
+              Chi tiết bài kiểm tra: "{test?.tieu_de}"
+            </CardTitle>
+            <CardDescription className='text-md text-white'>
+              {test?.mo_ta}
+            </CardDescription>
+            <div className='flex flex-wrap gap-2 items-center mt-2'>
               <Tag size={17} className='' />
               {danh_muc && danh_muc.map((item: any, index: number) =>
                 <Badge key={index} className=''>{item}</Badge>
               )}
             </div>
-            <p className="mt-2 text-gray-600 dark:text-gray-400 max-w-3xl">{test?.mo_ta}</p>
           </div>
           <div>
             <Button onClick={() => router.push(`/tests/${test_id}/edit`)} className='cursor-pointer rounded-[3px]'>Chỉnh sửa</Button>
@@ -272,17 +160,68 @@ const ExamDetailsPage = ({ test_id }: TestDetailProps) => {
                 </p>
               )}
             </div>
-
             <div className="mt-6">
-              {filteredQuestions?.map((q, i) => (
+              {mergedSections.length > 0 && (
+                <Tabs defaultValue={mergedSections[0]?.ma_phan?.toString()} className=''>
+                  <TabsList className='rounded-[3px] bg-gray-200'>
+                    {mergedSections.map((section, index) => (
+                      <TabsTrigger key={index} value={section?.ma_phan?.toString()} className='rounded-[3px] cursor-pointer'>
+                        {section.ten_phan}
+                      </TabsTrigger>
+                    ))}
+                  </TabsList>
+                  {mergedSections.map((section, index) => (
+                    <TabsContent key={index} value={section?.ma_phan?.toString()}>
+                      <div className='space-y-3'>
+                        <div className='px-4 py-4 bg-gray-200'>
+                          <div className='space-y-1'>
+                            <span className='font-bold'>Tên phần: </span>
+                            {section.ten_phan}
+                          </div>
+                          <div>
+                            <span className='font-bold'>Mô tả: </span>
+                            {section.mo_ta}
+                          </div>
+                        </div>
+                        <div>
+                          <DetailQuestionList
+                            questions={section.cau_hoi}
+                          />
+                        </div>
+                      </div>
+                    </TabsContent>
+                  ))}
+                </Tabs>
+              )}
+              {/* <Tabs defaultValue={mergedSections[0]?.ma_phan?.toString()}>
+                <TabsList>
+                  {mergedSections.map((section, index) => (
+                    <TabsTrigger key={index} value={section?.ma_phan?.toString()}>
+                      {section.ten_phan}
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+                {mergedSections.map((section, index) => (
+                  <TabsContent key={index} value={section?.ma_phan?.toString()}>
+                    <DetailQuestionList
+                      questions={section.cau_hoi}
+                    />
+                  </TabsContent>
+                ))}
+              </Tabs> */}
+
+              {/* {filteredQuestions?.map((q, i) => (
                 <QuestionCard
                   key={q.ma_cau_hoi}
                   question={q}
                   index={test?.cau_hoi.findIndex(originalQ => originalQ.ma_cau_hoi === q.ma_cau_hoi)}
                   searchTerm={searchTerm}
                 />
-              ))}
+              ))} */}
             </div>
+
+
+
           </main>
 
           {/* Sidebar: Exam Info */}
