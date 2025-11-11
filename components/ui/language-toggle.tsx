d0f62d573af002200985c0ec50db9342a6eb4be0
Hungnhatit <nhatpro204@gmail.com>
2025-11-11 12:18:26 +0700

    ui: update styles

--- components/ui/language-toggle.tsx
+++ components/ui/language-toggle.tsx
@@ -8,10 +8,9 @@ import { useI18n } from "@/providers/i18n-provider"
 export function LanguageToggle() {
   const { language, setLanguage, t } = useI18n();
   
-
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="cursor-pointer">
         <Button variant="ghost" size="icon">
           <Languages className="h-[1.2rem] w-[1.2rem]" />
           <span className="sr-only">{t.language}</span>
