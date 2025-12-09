d95cba7fef94b66c136581c4774b7badd9de244c
Hungnhatit <nhatpro204@gmail.com>
2025-12-09 21:54:19 +0700

    feat: parse data to test performance insight component

--- components/test-result/detail/test-performance-insights.tsx
+++ components/test-result/detail/test-performance-insights.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
-import { TrendingUp, TrendingDown, Target, Lightbulb } from 'lucide-react';
+import { TrendingUp, TrendingDown, Target, Lightbulb, BicepsFlexed, ChartNoAxesGantt, CircleSlash, CircleFadingArrowUp, AlarmClockCheck, CalendarCheck2 } from 'lucide-react';
+import { PhanTichDanhGia } from '@/types/interfaces/ai-review';
 
 interface Insight {
   type: 'strength' | 'improvement' | 'recommendation';
@@ -9,81 +10,158 @@ interface Insight {
 }
 
 interface PerformanceInsightsProps {
-  insights: Insight[];
-  strengths: string[];
-  improvements: string[];
+  performance: PhanTichDanhGia
 }
 
-export default function PerformanceInsights({ insights, strengths, improvements }: PerformanceInsightsProps) {
+export default function PerformanceInsights({ performance }: PerformanceInsightsProps) {
   return (
-    <Card className="shadow-lg">
-      <CardHeader className="pb-3">
-        <CardTitle className="flex items-center gap-2 text-lg">
-          <Lightbulb className="h-5 w-5 text-yellow-500" />
-          Phân tích hiệu suất
-        </CardTitle>
-        <CardDescription>
-          Phân tích chi tiết năng lực của bạn dựa trên kết quả bài thi, xác định các điểm mạnh nổi bật và kỹ năng cần phát triển thêm để nâng cao hiệu suất học tập.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-6">
-        <div className="grid md:grid-cols-2 gap-6">
-          <div>
-            <div className="flex items-center gap-2 mb-3">
-              <TrendingUp className="h-4 w-4 text-green-500" />
-              <p className="font-bold text-green-700 text-lg">Điểm mạnh</p>
-            </div>
-            <div className="space-y-2">
-              {strengths.map((strength, index) => (
-                <Badge key={index} variant="secondary" className="bg-green-100 text-green-800 hover:bg-green-200">
-                  {strength}
-                </Badge>
-              ))}
+    <div className='space-y-2'>
+      <Card className="shadow-lg">
+        <CardHeader className="">
+          <CardTitle className="flex items-center gap-2 text-lg">
+            <Lightbulb className="h-5 w-5 text-yellow-500" />
+            <h1>Phân tích hiệu suất</h1>
+          </CardTitle>
+          <CardDescription className='text-[16px]'>
+            Phân tích chi tiết năng lực của bạn dựa trên kết quả bài thi, xác định các điểm mạnh nổi bật và kỹ năng cần phát triển thêm để nâng cao hiệu suất học tập.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <div className="grid md:grid-cols-1 gap-6">
+            <Card className='gap-1'>
+              <CardHeader>
+                <CardTitle className='flex items-center gap-2 text-lg'>
+                  <ChartNoAxesGantt size={28} className="text-blue-500" />
+                  Tổng quan
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                {performance.tong_quan}
+              </CardContent>
+            </Card>
+
+            <div className='grid grid-cols-2 gap-4'>
+              <Card className='gap-1'>
+                <CardHeader>
+                  <CardTitle className='flex items-center gap-2 text-lg'>
+                    <BicepsFlexed className="text-green-500" />
+                    Điểm mạnh
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {performance.diem_manh}
+                </CardContent>
+              </Card>
+
+              <Card className='gap-1'>
+                <CardHeader>
+                  <CardTitle className='flex items-center gap-2 text-lg'>
+                    <CircleSlash size={24} className='text-orange-700' />
+                    Điểm yếu
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {performance.diem_yeu}
+                </CardContent>
+              </Card>
             </div>
           </div>
 
-          <div>
-            <div className="flex items-center gap-2 mb-3">
-              <Target className="h-4 w-4 text-orange-500" />
-              <h4 className="font-semibold text-orange-700">Các lĩnh vực cần cải thiện</h4>
-            </div>
-            <div className="space-y-2">
-              {improvements.map((improvement, index) => (
-                <Badge key={index} variant="secondary" className="bg-orange-100 text-orange-800 hover:bg-orange-200">
-                  {improvement}
-                </Badge>
-              ))}
-            </div>
-          </div>
-        </div>
+        </CardContent>
+      </Card>
 
-        <div className="space-y-4 pt-4 border-t">
-          <h4 className="font-semibold text-gray-900">
-            Phân tích chi tiết
-          </h4>
-          {insights.map((insight, index) => (
-            <div key={index} className="p-4 rounded-[3px] bg-gray-50 border-l-4 border border-blue-400  ">
-              <div className="flex items-start gap-3">
-                <div className={`p-1 rounded-full ${insight.type === 'strength' ? 'bg-green-100' :
-                  insight.type === 'improvement' ? 'bg-orange-100' : 'bg-blue-100'
-                  }`}>
-                  {insight.type === 'strength' ? (
-                    <TrendingUp className="h-4 w-4 text-green-600" />
-                  ) : insight.type === 'improvement' ? (
-                    <TrendingDown className="h-4 w-4 text-orange-600" />
-                  ) : (
-                    <Lightbulb className="h-4 w-4 text-blue-600" />
-                  )}
+      <Card className='gap-3'>
+        <CardHeader>
+          <CardTitle className='text-lg'>Đề xuất cải thiện</CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <div className="p-4 rounded-[3px] bg-gray-50 border-l-4 border border-blue-400  ">
+              <div className="flex items-center gap-3">
+                <div className={``}>
+                  <CircleFadingArrowUp className='text-green-600' />
+                  {/* <TrendingUp className="h-4 w-4 text-green-600" />
+                  <TrendingDown className="h-4 w-4 text-orange-600" />
+                  <Lightbulb className="h-4 w-4 text-blue-600" /> */}
                 </div>
                 <div>
-                  <h5 className="font-medium text-gray-900 mb-1">{insight.title}</h5>
-                  <p className="text-sm text-muted-foreground">{insight.description}</p>
+                  <h5 className="font-medium text-gray-900 mb-1">{performance.de_xuat_cai_thien}</h5>
+                  {/* <p className="text-sm text-muted-foreground">{insight.description}</p> */}
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card className='gap-3'>
+        <CardHeader>
+          <CardTitle className='text-lg'>Hướng phát triển</CardTitle>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <div className="p-4 rounded-[3px] bg-gray-50 border-l-4 border border-blue-400  ">
+              <div className="flex items-center gap-3">
+                <div className={`p-1 rounded-full `}>
+                  <TrendingUp className="text-green-600" />
+                </div>
+                <div>
+                  <h5 className="font-medium text-gray-900 mb-1">{performance.huong_phat_trien}</h5>
+                  {/* <p className="text-sm text-muted-foreground">{insight.description}</p> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className='grid grid-cols-2 gap-3'>
+        <Card className='gap-3'>
+          <CardHeader>
+            <CardTitle className='text-lg'>Kế hoạch ngắn hạn</CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <div className="p-4 rounded-[3px] bg-gray-50 border-l-4 border border-blue-400  ">
+                <div className="flex items-start gap-3">
+                  <div className={`p-1 rounded-full `}>
+                    <AlarmClockCheck className='text-sky-700' />
+                  </div>
+                  <div>
+                    <h5 className="font-medium text-gray-900 mb-1">{performance.ke_hoach_ngan_han}</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className='gap-3'>
+          <CardHeader>
+            <CardTitle className='text-lg'>Kế hoạch dài hạn</CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <div className="p-4 rounded-[3px] bg-gray-50 border-l-4 border border-blue-400  ">
+                <div className="flex items-start gap-3">
+                  <div className={`p-1 rounded-full `}>
+                    <CalendarCheck2 className='text-purple-700' />
+                  </div>
+                  <div>
+                    <h5 className="font-medium text-gray-900 mb-1">{performance.ke_hoach_dai_han}</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>      
+
+    </div>
+
   );
 }
