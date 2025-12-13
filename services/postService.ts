7207caf2004302c964fd51b1160e61aee156eadc
Hungnhatit <nhatpro204@gmail.com>
2025-12-13 16:09:24 +0700

    feat (service): add post service for frontend

---
+++ services/postService.ts
@@ -0,0 +1,63 @@
+import { api } from "@/lib/axios";
+
+interface CreatePostPayload {
+  ma_tac_gia: number | string;
+  tieu_de: string;
+  tom_tat?: string;
+  noi_dung?: string;
+  trang_thai?: string;
+  danh_muc?: string;
+  anh_bia?: File | null;
+  files: File[]
+}
+
+export const PostService = {
+  createPost: async (payload: CreatePostPayload) => {
+    const formData = new FormData();
+    const queryParams = new URLSearchParams();
+
+    formData.append('tieu_de', payload.tieu_de);
+    if (payload.tom_tat) formData.append('tom_tat', payload.tom_tat);
+    if (payload.noi_dung) formData.append('noi_dung', payload.noi_dung);
+    if (payload.trang_thai) formData.append('trang_thai', payload.trang_thai);
+    if (payload.danh_muc) formData.append('danh_muc', payload.danh_muc);
+    if (payload.anh_bia) {
+      formData.append('anh_bia', payload.anh_bia);
+    }
+    if (payload.files && payload.files.length > 0) {
+      payload.files.forEach((file) => {
+        formData.append('files', file);
+      })
+    }
+
+    try {
+      const res = await api.post(`/post/create`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      });
+
+      return res.data;
+    } catch (error: any) {
+      throw error.res?.data || error.message;
+    }
+  },
+
+  getPostByInstructorId: async (instructor_id: number, params: { page?: number; limit?: number; q?: string; trang_thai?: string }) => {
+    try {
+      const res = await api.get(`/post/instructor/${instructor_id}`, {
+        params: {
+          page: params.page,
+          limit: params.limit,
+          q: params.q,
+          trang_thai: (params.trang_thai && params.trang_thai !== 'all') ? params.trang_thai : undefined,
+          ma_tac_gia: instructor_id
+        }
+      });
+      return res.data;
+    } catch (error: any) {
+      throw error.res?.data || error.message;
+    }
+  }
+
+}
