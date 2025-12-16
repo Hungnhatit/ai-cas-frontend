c2b04bea91e6e38897b3f623e5308838d63c5a7e
Hungnhatit <nhatpro204@gmail.com>
2025-12-16 21:14:18 +0700

    feat (page): add about-us page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,13 @@
+import { AboutUs } from '@/components/about-us/AboutUs'
+import { LandingLayout } from '@/components/layout/landing-layout'
+import React from 'react'
+
+const AboutUsPage = () => {
+  return (
+    <LandingLayout>
+      <AboutUs />
+    </LandingLayout>
+  )
+}
+
+export default AboutUsPage
