f60719abce75a82c26cadb5b6a0c306b9e235cdc
Hungnhatit <nhatpro204@gmail.com>
2025-12-20 22:37:07 +0700

    feat (service): add get review by ID service

--- services/ai-review/AIReviewService.ts
+++ services/ai-review/AIReviewService.ts
@@ -5,5 +5,12 @@ export const AiReviewService = {
   getAiReviewById: async (result_id: number) => {
     const res = await api.get(`/ai/${result_id}/result`);
     return res.data;
+  },
+
+  getReviewByAttemptID: async (attempt_id: number) => {
+    const res = await api.get(`/ai/result/attempt/${attempt_id}`);
+    return res.data;
   }
+
+
 }
